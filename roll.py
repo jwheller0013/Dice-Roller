a968b1f0d5fcbca4740e65f2ee47751a305b31a0
jwheller0013 <jwheller0013@yahoo.com>
2025-03-12 19:48:54 -0400

    Rolls dice seperate sets

--- roll.py
+++ roll.py
@@ -4,4 +4,21 @@
 # roll 2 - produce two numbers 1-6 and print them
 # roll 3 - produce three numbers and print them.
 #
+from random import randrange
 
+
+def roll_dice ():
+    yes = True
+    while yes == True:
+        numberOfDice = input("How many dice would you like to roll? \n")
+        results = []
+        for i in range(int(numberOfDice)):
+            x= int(randrange(1,7))
+            results.append(x)
+        print(results)
+        playAgain = input("Roll more?\n")
+        if playAgain == "No":
+            yes = False
+
+
+roll_dice()
